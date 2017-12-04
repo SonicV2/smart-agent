ac9596e1acca262d4e01fc935f8b804e5d39da0a
Andrea Gagliardi La Gala <alagala@fedora.home>
2017-12-04 23:10:21 +0800

    Added the ability to create notes attachments

--- client-app/src/libs/awsLib.js
+++ client-app/src/libs/awsLib.js
@@ -22,8 +22,14 @@ export async function authUser() {
 
 export function signOutUser() {
     const currentUser = getCurrentUser();
+
     if (currentUser !== null) {
         currentUser.signOut();
+    }
+
+    if (AWS.config.credentials) {
+        AWS.config.credentials.clearCacheId();
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({});
     }
 }
 
@@ -61,6 +67,27 @@ export async function invokeAPI({
     }
 
     return results.json();
+}
+
+export async function s3Upload(file) {
+    if (!await authUser()) {
+        throw new Error("User is not logged in");
+    }
+
+    const s3 = new AWS.S3({
+        params: {
+            Bucket: config.s3.BUCKET
+        }
+    });
+
+    const filename = `${AWS.config.credentials.identityId}-${Date.now()}-${file.name}`;
+
+    return s3.upload({
+        Key: filename,
+        Body: file,
+        ContentType: file.type,
+        ACL: "public-read"
+    }).promise();
 }
 
 function getUserToken(currentUser) {
